bed591828fbcd79ccbc213cb2f96ee390c5b9ffc
mchammer91 <yankeesbatboy91@gmail.com>
2024-10-22 12:50:57 -0500

    added project

---
+++ SimpleStack/Program.cs
@@ -0,0 +1,17 @@
+﻿var stack = new SimpleStack<double>();
+
+stack.Push(1.2);
+stack.Push(2.3);
+stack.Push(3.4);
+
+var sum = 0d;
+
+while (stack.Count > 0)
+{
+    var item = stack.Pop();
+    System.Console.WriteLine($"Item: ${item}");
+    sum += item;
+}
+
+System.Console.WriteLine($"Sum: ${sum}");
+System.Console.ReadLine();
---
+++ SimpleStack/SimpleStack.cs
@@ -0,0 +1,12 @@
+﻿public class SimpleStack<T>
+{
+    private readonly T[] _items = new T[10];
+    private int _currentIndex = -1;
+    public int Count => _currentIndex + 1;
+
+    // Prefix operator to increment before assigning to _currentIndex and then assigning the value to the array
+    public void Push(T v) => _items[++_currentIndex] = v;
+
+    // Postfix operator to assign the value to the array and then decrement _currentIndex
+    public T Pop() => _items[_currentIndex--];
+}
