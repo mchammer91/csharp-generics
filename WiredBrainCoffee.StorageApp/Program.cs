c17e82f3ffa14388dd67b6cba1cfa253bd2c35cf
mchammer91 <yankeesbatboy91@gmail.com>
2024-11-13 14:52:40 -0600

    Created generic extension method

--- WiredBrainCoffee.StorageApp/Program.cs
+++ WiredBrainCoffee.StorageApp/Program.cs
@@ -55,96 +55,19 @@ void AddEmployees(IRepository<Employee> repository)
     
     var employees = new[] { julia, anna, thomas };
     
-    // repository.Add(julia);
-    // repository.Add(anna);
-    // repository.Add(thomas);
-    // repository.Save();
-    
-    AddBatch(repository, employees);
+    /*
+     * Fun fact: could also be called as static method like so:
+     * RepositoryExtensions.AddBatch(repository, organizations);
+     */
+    repository.AddBatch(employees);
 }
 
-void AddOrganizations(IRepository<Organization> repository)
+void AddOrganizations(IWriteRepository<Organization> repository)
 {
     var pluralsight = new Organization { Name = "Pluralsight" };
     var globometrics = new Organization { Name = "Globomantics" };
     
     var organizations = new[] { pluralsight, globometrics };
     
-    // repository.Add(pluralsight);
-    // repository.Add(globometrics);
-    // repository.Save();
-    
-    AddBatch(repository, organizations);
+    repository.AddBatch(organizations);
 }
-
-/*
- * First attempt: this doesn't work because IRepository<IEntity> is invariant
- * so can't use Employee or Organization as generic type parameter
- */
-// void AddBatch(IRepository<IEntity> repository, IEntity[] employees)
-// {
-//     foreach (var employee in employees) 
-//     {
-//         repository.Add(employee);
-//     }
-//     
-//     repository.Save();
-// }
-
-/*
- * Second attempt: although this is better in one sense because IReadRepository<IEntity> is
- * covariant, it won't work because IReadRepository doesn't have the Add or Save methods
- * defined on it
- */
-// void AddBatch(IReadRepository<IEntity> repository, IEntity[] employees)
-// {
-//     foreach (var employee in employees) 
-//     {
-//         repository.Add(employee);
-//     }
-//     
-//     repository.Save();
-// }
-
-/*
- * Third attempt: although this is better in one sense because IWriteRepository<IEntity> has
- * the Add and Save methods defined, it won't work because IWriteRepository is contravariant
- */
-// void AddBatch(IWriteRepository<IEntity> repository, IEntity[] employees)
-// {
-//     foreach (var employee in employees) 
-//     {
-//         repository.Add(employee);
-//     }
-//     
-//     repository.Save();
-// }
-
-/*
- * Final attempt: by making the method take a generic type parameter, we can mitigate all
- * the issues of the previous attempts.
- */
-void AddBatch<T>(IWriteRepository<T> repository, T[] employees)
-{
-    foreach (var employee in employees) 
-    {
-        repository.Add(employee);
-    }
-    
-    repository.Save();
-}
-
-/*
- * Note: we could have used IRepository<T> rather than IWriteRepository but that would have
- * required adding `where T : IEntity`
- * Concern: IWriteRepository doesn't have the `where T : IEntity` type constraint on it, but all
- * implementations inherit from IRepository - think through this more
- * Concern: how are we able to get around the type constraints of the interface argument by
- * using a generic method? Seems like that allows room for potentially breaking the
- * contract created by the interface and its generic type parameter in the first place...
- */
-
-/*
- * The solution is that both ListRepository and SqlRepository have their generic type constraints
- * specified to adhere to the IRepository interface's constraints
- */
---
+++ WiredBrainCoffee.StorageApp/Repositories/RepositoryExtensions.cs
@@ -0,0 +1,14 @@
+﻿namespace WiredBrainCoffee.StorageApp.Repositories;
+
+public static class RepositoryExtensions
+{
+    public static void AddBatch<T>(this IWriteRepository<T> repository, T[] employees)
+    {
+        foreach (var employee in employees) 
+        {
+            repository.Add(employee);
+        }
+    
+        repository.Save();
+    }
+}
