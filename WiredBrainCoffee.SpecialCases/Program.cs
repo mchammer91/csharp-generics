4fbb596cc4d712df3ded722b07581ff1d6b44e7c
mchammer91 <yankeesbatboy91@gmail.com>
2024-11-26 09:18:30 -0600

    - Demo generic method in generic class

--- WiredBrainCoffee.SpecialCases/Program.cs
+++ WiredBrainCoffee.SpecialCases/Program.cs
@@ -6,7 +6,8 @@ _ = new Container<int>();
 
 /*
  * The static InstanceCount property only tracks the total number of invocations
- * for each generic type parameter used.
+ * for each generic type parameter used. Container<string> is considered to be a different type
+ * than Container<int>. 
  */
 Console.WriteLine($"Container<string>: {Container<string>.InstanceCount}"); // 2
 Console.WriteLine($"Container<int>: {Container<int>.InstanceCount}"); // 1
@@ -16,10 +17,22 @@ Console.WriteLine($"Container<bool>: {Container<bool>.InstanceCount}"); // 0
 // Console.WriteLine($"ContainerBase: {Container<string>.InstanceCountBase}"); // 3
 Console.WriteLine($"ContainerBase: {ContainerBase.InstanceCountBase}"); // 3
 
+var c = new Container<int>();
+c.PrintItem<string>("Hello");
+
 class Container<T> : ContainerBase
 {
     public Container() => InstanceCount++;
     public static int InstanceCount { get; private set; }
+    
+    /*
+     * We use a different name for the method's generic type argument to prevent from
+     * hiding the class's generic type parameter. Could also leave <TItem> out of the
+     * method definition to use the class's generic type parameter T in the method definition.
+     * Good to know that a generic method can accept a different generic type argument than the
+     * class's generic type parameter. 
+     */
+    public void PrintItem<TItem>(TItem item) => Console.WriteLine($"Item: {item}");
 }
 
 /*
